1b236bb51af62f488a767017c632c01a30ea1181
Marcos-Echeverria <marcosalbertoecheverria@gmail.com>
2023-07-26 21:20:03 -0300

    first commit

---
+++ nestfirstapp/src/tasks/dto/task.dto.ts
@@ -0,0 +1,13 @@
+import { TaskStatus } from "../task.entity"
+
+export class CreateTaskDto {
+    title: string;
+    description: string;
+    
+}
+
+export class updateTaskDto {
+    title: string;
+    description: string;
+    status?: TaskStatus;
+}
