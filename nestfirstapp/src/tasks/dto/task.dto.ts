e909b5bdc646330f232563ed06e14282128939f2
Marcos-Echeverria <marcosalbertoecheverria@gmail.com>
2023-07-27 12:20:27 -0300

    Validaciones en peticiones

--- nestfirstapp/src/tasks/dto/task.dto.ts
+++ nestfirstapp/src/tasks/dto/task.dto.ts
@@ -1,13 +1,28 @@
 import { TaskStatus } from "../task.entity"
+import {IsString, IsNotEmpty, MinLength, IsOptional, IsIn } from 'class-validator'
 
 export class CreateTaskDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
     title: string;
+
+    @IsString()
     description: string;
     
 }
 
 export class updateTaskDto {
+    @IsString()
+    @IsOptional()
     title: string;
+
+    @IsString()
+    @IsOptional()
     description: string;
+
+    @IsString()
+    @IsOptional()
+    @IsIn([TaskStatus.IN_PROGRESS, TaskStatus.DONE, TaskStatus.PENDING])
     status?: TaskStatus;
 }
